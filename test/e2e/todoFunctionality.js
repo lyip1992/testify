b95fc77721df4142f411f7ee5cb93aac48a959b4
Leon Yip <lyip1992@gmail.com>
2015-10-30 10:38:24 -0700

    (refactor) remove whitespace

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -1,11 +1,11 @@
 /* globals casper, document */
-casper.test.begin('App is setup correctly', 2, function suite(test) {
-  casper.start('http://localhost:3000/', function() {
+casper.test.begin('App is setup correctly', 2, function suite(test){
+  casper.start('http://localhost:3000/', function(){
     test.assertExists('.todo-list', 'List should exist');
     test.assertExists('.todo-form', 'Form should exist');
   });
 
-  casper.run(function() {
+  casper.run(function(){
     test.done();
   });
 });
