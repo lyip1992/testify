fe9373265ddd7de16c167e01e9660a2746b3f559
Leon Yip <lyip1992@gmail.com>
2015-10-30 10:14:28 -0700

    (test) add stub

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,17 @@
 describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
+  before(function(){
+    JSONresponse = {
+      todos: [
+        { name: 'Test1', done: true },
+        { name: 'Test2', done: false },
+        { name: 'Test3', done: true }
+      ]
+    };
+    setupStub = sinon.stub(todo, 'setup');
+  });
+
   it('todo.setup receives an array of todos when todo.init is called', function () {
   });
 });
