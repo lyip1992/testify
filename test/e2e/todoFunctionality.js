9fa786a490fd129d5ad885f0fbaddfe8b66c9f81
Leon Yip <lyip1992@gmail.com>
2015-10-30 11:07:39 -0700

    (test) list item should not be added for empty todos

--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -93,5 +93,7 @@ casper.test.begin('Does not add empty todo items', 1, function suite(test){
   casper.start('http://localhost:3000/', function(){
     // Submit the form without filling it out
     this.fill('form.todo-form', {}, true);
+
+    test.assertDoesntExist('.todo-list .todo-item', 'List item should not be added for empty todos');
   });
 });
